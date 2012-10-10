ac93533f2d28a430a5f313e0b4b08a3ba8e22828
Thomas Wilburn <tbone@thomaswilburn.net>
2012-10-10 12:23:24 -0700

    Rewrite to use Bag collections and to simplify get/set/ask/cue/response flow significantly.

--- World.js
+++ World.js
@@ -1,147 +1,346 @@
-var Entity = function(world) {
-  this.name = "";
+(function() {
+
+/*
+
+Bag is effectively a mini underscore.js - a collection type that allows for
+filtering and mapped get/set operations. Many operations on Bags are
+chainable. You can also query Bags by property values, similar to a bracket-
+less CSS attribute selector.
+
+*/
+var Bag = function(array) {
+  if (!(this instanceof Bag)) return new Bag(array);
+  this.items = array || [];
+};
+Bag.prototype.push = function() {
+  this.items.push(Array.prototype.slice.call(arguments));
+};
+Bag.prototype.remove = function(item) {
+  var remaining = [];
+  for (var i = 0; i < this.items.length; i++) {
+    if (this.items[i] != item) remaining.push(this.items[i]);
+  }
+  this.items = remaining;
+};
+Bag.prototype.getAt = function(n) {
+  return this.items[n];
+};
+Bag.prototype.filter = function(f) {
+  var filtered = [];
+  for (var i = 0; i < this.items.length; i++) {
+    if (f(this.items[i])) filtered.push(this.items[i]);
+  }
+  return new Bag(filtered);
+}
+Bag.prototype.map = function(f) {
+  var mapped = this.items.map(f);
+  return new Bag(mapped);
+};
+Bag.prototype.mapGet = function(p) {
+  return this.items.map(function(item) {
+    return item[p];
+  });
+};
+Bag.prototype.mapSet = function(p, value) {
+  for (var i = 0; i < this.items.length; i++) {
+    this.items[i][p] = value;
+  }
+};
+Bag.prototype.invoke = function(name) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  var map = [];
+  for (var i = 0; i < this.items.length; i++) {
+    var item = this.items[i];
+    map[i] = item[name].apply(item, args);
+  }
+}
+Bag.prototype.each = function(f) {
+  for (var i = 0; i < this.items.length; i++) {
+    f(this.items[i]);
+  }
+};
+Bag.prototype.toArray = function() {
+  return this.items;
+};
+/*
+
+We only query on attributes--it saves selector complexity. The supported
+selector operators are:
+  =   equals
+  >   greater than
+  >=  greater than or equal to
+  <   less than
+  <=  less than or equal to
+  !=  not equal to
+  ?  truthiness
+  ~=  array contains
+  ^=  string begins
+  $=  string ends
+  *=  string contains
+
+Multiple selectors can be passed in using a comma. These act as AND operators,
+not OR, which is different from CSS but necessary since we're skipping the
+brackets.
+
+*/
+Bag.prototype.query = function(selectors) {
+  selectors = selectors.split(',');
+  var matcher = '^\\s*(\\w+)\\s*([<>!~$*^?=]{0,2})\\s*\\"{0,1}([^\\"]*)\\"{0,1}\\s*$';
+  var tests = {
+    '=': function(a, b) { return a === b },
+    '>': function(a, b) { return a > b },
+    '>=': function(a, b) { return a >= b },
+    '<': function(a, b) { return a <= b },
+    '!=': function(a, b) { return a !== b },
+    '?': function(a) { return a },
+    '~=': function(a, b) {
+      if (typeof a.length == 'undefined') return false;
+      if (typeof Array.prototype.indexOf != 'undefined') {
+        return a.indexOf(b) != -1;
+      } else {
+        for (var i = 0; i < a.length; i++) {
+          if (a[i] == b) return true;
+        }
+        return false;
+      }
+    },
+    '^=': function(a, b) {
+      if (typeof a != 'string') return false;
+      return a.search(b) == 0
+    },
+    '$=': function(a, b) {
+      if (typeof a != 'string') return false;
+      return a.search(b) == a.length - b.length
+    },
+    '*=': function(a, b) {
+      if (typeof a != 'string') return false;
+      return a.search(b) != -1
+    },
+    fail: function() { return false }
+  }
+  for (var i = 0; i < selectors.length; i++) {
+    var parts = new RegExp(matcher).exec(selectors[i]);
+    if (!parts) throw('Bad selector: ' + selectors[i]);
+    selectors[i] = {
+      key: parts[1],
+      operator: parts[2]
+    };
+    var value = parts[3].replace(/^\s*|\s*$/g, '');
+    if (value == "true" || value == "false") {
+      value = value == "true";
+    } else if (value != "" && !isNaN(value)) {
+      value = parseFloat(value);
+    }
+    selectors[i].value = value;
+  };
+  var passed = [];
+  for (var i = 0; i < this.items.length; i++) {
+    var item = this.items[i];
+    var hit = true;
+    for (var j = 0; j < selectors.length; j++) {
+      var s = selectors[j];
+      if (typeof item[s.key] == 'undefined') {
+        hit = false;
+        break;
+      } else if (s.operator) {
+        var f = tests[s.operator] || tests.fail;
+        if (!f(item[s.key], value)) {
+          hit = false;
+          break;
+        }
+      }
+    }
+    if (hit) {
+      passed.push(item);
+    }
+  }
+  return new Bag(passed);
+};
+
+/*
+
+The base type for all other objects in the World is the Thing. You extend off
+from Thing by calling Thing.mutate() and passing in a type ID string and a
+constructor function unique to your type (both of these are optional). Then
+you can add properties to your new prototype at your discretion.
+
+Things come with some basic shared utility methods:
+
+  - get() - returns a property or function value by key. Similar to _.result()
+
+  - proxy() - lets you intercept calls to get() and interfere with them. Useful
+    for creating "private" properties, as well as for temporarily overriding
+    certain rules.
+
+  - cue() - sets up an action event response. See also:
+
+  - ask() - This is similar to get(), but ask() is meant to be used for user-
+    facing events, while get() is meant to construct your own internal APIs.
+    ask() should return a string, while get() can return anything.
+
+  - nudge() - feed this an object string from the parser, and it will respond
+    true/false depending on whether the object "answers" to that name.
+
+  - say() - output to the browser or UI console via the World. Basically used
+    as a local output method.
+
+*/
+var Thing = function(world) {
   this.classes = [];
-  this.actions = {};
-  this.cues = {};
+  this.proxy = {};
+  this.cues = {
+    'look': function() {
+      return this.description;
+    }
+  };
+
   if (world) {
     this.world = world;
     world.things.push(this);
   }
 };
-Entity.prototype = {
+Thing.prototype = {
+  name: "",
+  description: "",
   get: function(key) {
     if (!this[key]) return null;
+    if (this.proxy[key]) {
+      return this.proxy[key].call(this);
+    }
     if (typeof this[key] == 'function') {
       return this[key]();
     } else {
       return this[key];
     }
   },
-  act: function(action, event) {
-    if (!this.actions[action]) return false;
-    this.actions[action].call(this, event);
+  proxy: function(key, f) {
+    this.proxy[key] = f;
   },
-  on: function(action, callback) {
-    this.actions[action] = callback;
-  },
-  say: function(key) {
+  ask: function(key) {
     if (!this.cues[key]) return "";
     if (typeof this.cues[key] == 'function') {
-      return this.cues[key]();
+      var response = this.cues[key].call(this);
+      return response;
     } else {
-      return this.cues[key];
+      var response = this.cues[key];
+      if (this.world) this.world.print(response);
+      return response;
     }
   },
   cue: function(key, value) {
     this.cues[key] = value;
+  },
+  say: function(response) {
+    if (this.world) {
+      this.world.print(response);
+    } else {
+      console.log(response);
+    }
+  },
+  nudge: function(input) {
+    return /override this/.test(input);
   }
 }
 
-Entity.mutate = function(f) {
+Thing.mutate = function(tag, f) {
+  if (typeof tag == 'function') {
+    f = tag;
+    tag = 'Thing';
+  }
   f = f || function() {};
-  var C = function(world) {
-    if (!(this instanceof C)) {
-      if (this instanceof World) return new C(this);
+  var Type = function(world) {
+    if (!(this instanceof Type)) {
+      if (this instanceof World) return new Type(this);
       return new C();
     }
-    Entity.call(this);
+    Thing.call(this);
     f.call(this);
     if (world) {
       this.world = world;
       world.things.push(this);
     }
   };
-  C.prototype = new Entity();
-  return C;
+  Type.prototype = new Thing();
+  Type.prototype.type = tag;
+  return Type;
 };
 
-var Region = Entity.mutate(function() {
-  this.rooms = [];
+var Region = Thing.mutate('Region', function() {
+  this.rooms = new Bag();
 });
 
-var Room = Entity.mutate(function() {
-  this.contents = [];
-})
-Room.prototype.contains = function(o) {
-  this.contents.push(o);
-  o.act('entered', {subject: this});
+var Room = Thing.mutate('Room', function() {
+  this.contents = new Bag();
+  this.cue('contents', function() {
+    var indent = '\n  ';
+    return indent + this.get('contents').query('[background=false]').getAll('long').join(indent);
+  });
+  this.cue('look', function() {
+    this.say('description');
+    this.say(this.ask('contents'));
+  });
+});
+Room.prototype.add = function(item) {
+  this.contents.push(item);
+  item.parent = this;
 };
-
-var Thing = Entity.mutate();
-
-var Person = Entity.mutate();
-
-var Player = Entity.mutate();
-
-var Container = Entity.mutate(function() {
-  this.contents = [];
-});
-Container.prototype.contains = function(o) {
-  this.contents.push(o);
-  o.act('inserted', {subject: this});
+Room.prototype.remove = function(item) {
+  this.contents.remove(item);
+  item.parent = null;
 }
 
-var Supporter = Entity.mutate(function() {
+var Container = Thing.mutate('Container', function() {
+  this.contents = new Bag();
+  this.open = false;
+});
+Container.prototype.add = function(item) {
+  this.contents.push(item);
+  item.parent = this;
+};
+Container.prototype.remove = function(item) {
+  this.contents.remove(item);
+  item.parent = null;
+}
+
+
+var Person = Thing.mutate('Person');
+
+var Player = Thing.mutate('Player');
+
+var Supporter = Thing.mutate('Supporter', function() {
   this.under = [];
 });
 
-var Scenery = Entity.mutate();
-Scenery.prototype.enumerate = false;
-
-var match = {
-  TYPE: "^([A-Z][a-zA-Z]*)",
-  CLASS: "\\.([\\w-]+)",
-  ID: "#([\\w-]+)",
-  ATTRIBUTE: '\\[([\\w-]+)\\s*([!<>=]{1,2})\\s*\"*([\\w-]+)\\"*\\]'
-};
-
-var check = function(selector, item) {
-  var typeTest = new RegExp(match.TYPE).exec(selector);
-  if (typeTest) {
-    var className = typeTest[1];
-    var isType = new Function("item", "return typeof " + className + " != 'undefined' && item instanceof " + className);
-    if (!isType(item)) return false;
-  }
-  return true;
-};
-
-var Filterable = function(array) {
-  if (!(this instanceof Filterable)) return new Filterable(array);
-  this.items = array;
-};
-Filterable.prototype.find = function(selector) {
-  var filtered = [];
-  for (var i = 0; i < this.items.length; i++) {
-    var item = this.items[i];
-    if (check(selector, item)) {
-      filtered.push(item);
-    }
-  }
-  return new Filterable(filtered);
-};
-Filterable.prototype.get = function() {
-  return this.items;
-};
-Filterable.prototype.make = function(key, value) {
-  for (var i = 0; i < this.items.length; i++) {
-    this.items[i][key] = value;
-  }
-};
+var Scenery = Thing.mutate('Scenery', function() {
+  this.background = true;
+  this.movable = false;
+});
 
 var World = function() {
   this.things = [];
   this.player = null;
 };
 World.prototype = {
+  Bag: Bag,
+  Thing: Thing.mutate('Thing'),
   Region: Region,
   Room: Room,
   Player: Player,
-  Thing: Thing,
   Container: Container,
   Supporter: Supporter,
   Scenery: Scenery,
   parse: function() {},
-  find: function(selector) {
-
+  print: function(line) {
+    console.log(line);
   }
 };
+
+if (typeof define !== 'undefined') {
+  define(function() {
+    return World;
+  });
+} else {
+  window.World = World;
+}
+
+})();
--- test.js
+++ test.js
@@ -7,27 +7,20 @@ var field = zork.Room();
 field.description = "You are standing in an open field west of a white house, with a boarded front door.\nThere is a small mailbox here.";
 
 var mailbox = zork.Container();
-field.contains(mailbox);
+field.add(mailbox);
 mailbox.open = false;
-mailbox.name = "the small mailbox";
-mailbox.description = function() {
-  if (this.open) {
-    return this.say('contents');
-  } else {
-    return this.name + " is closed.";
-  }
-}
+mailbox.description = "It's painted red."
 
 var leaflet = zork.Thing();
-mailbox.contains(leaflet);
-leaflet.name = "a leaflet";
-leaflet.description = "WELCOME TO ZORK!\nZORK is a game of adventure, danger, and low cunning. In it you will explore some of the most amazing territory ever seen by mortals. No computer should be without one!";
+mailbox.add(leaflet);
+leaflet.cue('read', "WELCOME TO ZORK!\nZORK is a game of adventure, danger, and low cunning. In it you will explore some of the most amazing territory ever seen by mortals. No computer should be without one!");
+leaflet.description = "It looks like some kind of product pitch."
 
 var door = zork.Scenery();
-field.contains(door);
-door.name = 'door';
-door.actions.open = "The door cannot be opened.";
-door.actions.cross = "The door is boarded and you can't remove the boards."
+field.add(door);
+door.cue('open', "The door cannot be opened.");
+door.cue('cross', "The door is boarded and you can't remove the boards.");
+door.description = "It's all boarded up.";
 
 var player = zork.Player();
 player.location = field;
